daba25dd20d827c4ca6f39b79c71fd6422d7ded1
Alex Iribarren <Alex.Iribarren@cern.ch>
2013-07-21 00:16:59 +0200

    Fix imports

--- src/android/LiteGap.java
+++ src/android/LiteGap.java
@@ -1,9 +1,9 @@
 package com.couchbase.cblite.plugins;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.api.CordovaInterface;
+import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 
 import com.couchbase.cblite.CBLServer;
