09cf6b890be7d4fe55ea39327bb30e2f675ebad2
zebra1024 <users.noreply.github.com>
2016-02-18 10:24:24 -0500

    Fix issue #71 reported at https://github.com/couchbaselabs/Couchbase-Lite-PhoneGap-Plugin/issues/71 IOS Code fails to compile using Cordova 6 and IOS 4.
    Removed initWithWebView:(UIWebView*)theWebView as it no longer exists in Cordova 6.
    Moved launchCouchbaseLite call to pluginInitialize
    	modified:   src/ios/CBLite.m

--- src/ios/CBLite.m
+++ src/ios/CBLite.m
@@ -10,37 +10,33 @@
 
 @synthesize liteURL;
 
-- (id) initWithWebView:(UIWebView*)theWebView
+- (void)pluginInitialize
 {
-    self = [super initWithWebView:theWebView];
-    if (self) {
         // todo check domain whitelist to give devs a helpful error message
         [self launchCouchbaseLite];
-    }
-    return self;
 }
 
 - (void)getURL:(CDVInvokedUrlCommand*)urlCommand
 {
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[self.liteURL absoluteString]];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:urlCommand.callbackId];
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[self.liteURL absoluteString]];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:urlCommand.callbackId];
 }
 
 
 - (void)launchCouchbaseLite
 {
-    NSLog(@"Launching Couchbase Lite...");
-    CBLManager* dbmgr = [CBLManager sharedInstance];
-    CBLRegisterJSViewCompiler();
+        NSLog(@"Launching Couchbase Lite...");
+        CBLManager* dbmgr = [CBLManager sharedInstance];
+        CBLRegisterJSViewCompiler();
 #if 1
-    // Couchbase Lite 1.0's CBLRegisterJSViewCompiler function doesn't register the filter compiler
-    if ([CBLDatabase filterCompiler] == nil) {
-        Class cblJSFilterCompiler = NSClassFromString(@"CBLJSFilterCompiler");
-        [CBLDatabase setFilterCompiler: [[cblJSFilterCompiler alloc] init]];
-    }
+        // Couchbase Lite 1.0's CBLRegisterJSViewCompiler function doesn't register the filter compiler
+        if ([CBLDatabase filterCompiler] == nil) {
+                Class cblJSFilterCompiler = NSClassFromString(@"CBLJSFilterCompiler");
+                [CBLDatabase setFilterCompiler: [[cblJSFilterCompiler alloc] init]];
+        }
 #endif
-    self.liteURL = dbmgr.internalURL;
-    NSLog(@"Couchbase Lite url = %@", self.liteURL);
+        self.liteURL = dbmgr.internalURL;
+        NSLog(@"Couchbase Lite url = %@", self.liteURL);
 }
 
 @end
